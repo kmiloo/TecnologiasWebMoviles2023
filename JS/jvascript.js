7ae9e5c1a140398e09f306fcc2ea8749615da8e1
kmiloo <camiloandres.lovera@alumnos.ulagos.cl>
2023-09-07 12:09:54 -0300

    Html

--- JS/jvascript.js
+++ JS/jvascript.js
@@ -1,16 +1,16 @@
-var estatura = 1.71;
-let peso = 60;
-const nombre = "Victor";
+//var estatura = 1.71;
+//let peso = 60;
+//const nombre = "Victor";
 
-const n_grande = 1e6; //millon
-const n_pequeño = 1e-6; //0.000001
+//onst n_grande = 1e6; //millon
+//const n_pequeño = 1e-6; //0.000001
 
-const bigInt = 123456789012345678901234567890n;
+//const bigInt = 123456789012345678901234567890n;
 /////////////
-let string1 = "!hola ";
-let string2 = 'Beunas traddes';
-let frase = `Este es un saldo: ${string1}`;
+//let string1 = "!hola ";
+//let string2 = 'Beunas traddes';
+//let frase = `Este es un saldo: ${string1}`;
 /////////////
 alert("hola munnnndo");
-console.log("hola mundo");
-document.write();
+//console.log("hola mundo");
+//document.write();
