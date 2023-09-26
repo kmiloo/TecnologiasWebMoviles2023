c3d48167360c0eaf83850a6a10e802277622b7fc
kmiloo <camiloandres.lovera@alumnos.ulagos.cl>
2023-09-26 14:46:49 -0300

    .

---
+++ Clases/JS/jvascript.js
@@ -0,0 +1,94 @@
+//var estatura = 1.71;
+//let peso = 60;
+//const nombre = "Victor";
+
+//onst n_grande = 1e6; //millon
+//const n_pequeño = 1e-6; //0.000001
+
+//const bigInt = 123456789012345678901234567890n;
+/////////////
+//let string1 = "!hola ";
+//let string2 = 'Beunas traddes';
+//let frase = `Este es un saldo: ${string1}`;
+/////////////
+alert("hola munnnndo");
+//console.log("hola mundo");
+//document.write();
+
+/////////////////////////////
+//concatenacion +
+console.log(ciuduad + " y " + capital + " asdsa ");
+//,
+console.log(ciudad, " y ",capital, " asd " );
+
+
+//operadores
+
+//suma
+let tomates = 30
+let naranjas = 20
+console.log("La suma entre los tomastes y naranjas ses: ", tomates + naranjas);
+
+//suma2
+let caja = "2"
+let estantes = "5"
+console.log(+caja);
+
+suma = console.log("La suma entre caja y estantes es: "+caja + +estantes);
+
+//resta - multiplicacion * resto 6 % 2  potencia 2**2=4  
+
+//asignaciones encadenadas
+let a, b, c;
+a = b = c = 5 + 5;
+
+console.log(a,b,c);//10
+
+//modifiacion en el lugar
+let n = 1;
+n = n + 9 ;
+//n+= 5;
+//n*=2;
+
+
+////////////////////////
+//incremento decremnento
+
+let contador = 1;
+contador++;
+contador--;
+
+//////////////////
+//CONDICIONALEs////////////////////////
+
+let year = promt ("en que año se creo jvscript", " ")
+//if =comparativo ==operador  ===igualdad estricta
+if(year = 1995){
+    alert("correcto");
+} else{
+    alert("incorrecto");
+}
+
+if(edad >= 18){
+
+}else if(edad == 17){
+
+}else{
+
+}
+
+
+//
+[1, 2, 3, 4]:forEach(console-log);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Ejercicios/Ej1.js
@@ -0,0 +1,8 @@
+////////////////////////////////////////////////////
+//1) Crear un algoritmo que solicite al usuario un número entero positivo. Luego, utilizar un
+//bucle para calcular la suma de todos los números pares desde 1 hasta el número ingresado
+//por el usuario. Mostrar el resultado en el navegador.
+
+
+
+
