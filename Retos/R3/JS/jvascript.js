14622610d35b2e56f2ba536b2c4a7fcbdb777815
kmiloo <camiloandres.lovera@alumnos.ulagos.cl>
2023-10-03 21:24:42 -0300

    reto3

--- Retos/R3/JS/jvascript.js
+++ Retos/R3/JS/jvascript.js
@@ -1,6 +1,6 @@
 function Propina(){
     let numero = parseFloat(document.getElementById('boleta').value);
-    let porcentaje = parseFloat(document.getElementById('das').value);
+    let porcentaje = parseFloat(document.getElementById('porcentaje').value);
     let propina = numero*(porcentaje/100);
     document.getElementById('resultado').innerHTML = Math.trunc(propina);
     document.getElementById('total').innerHTML = Math.trunc(numero+propina);
