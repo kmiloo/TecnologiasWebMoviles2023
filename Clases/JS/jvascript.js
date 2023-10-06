393bbb3bdb89d20b1383aa46e04b8037cde3334d
kmiloo <camiloandres.lovera@alumnos.ulagos.cl>
2023-10-06 00:24:50 -0300

    JS

--- Clases/JS/jvascript.js
+++ Clases/JS/jvascript.js
@@ -1,94 +1,171 @@
+//hola mundo
+//alert("hola munnnndo");
+//console.log("hola mundo");
+//document.write();
+
+//variables
 //var estatura = 1.71;
 //let peso = 60;
 //const nombre = "Victor";
 
-//onst n_grande = 1e6; //millon
+//saber el tipo de dato
+// console.log(typeof peso);
+//let asd = toString(); parseFloat() parseInt()
+
+
+//numeros
+//const n_grande = 1e6; //millon
 //const n_pequeño = 1e-6; //0.000001
-
 //const bigInt = 123456789012345678901234567890n;
-/////////////
-//let string1 = "!hola ";
-//let string2 = 'Beunas traddes';
-//let frase = `Este es un saldo: ${string1}`;
-/////////////
-alert("hola munnnndo");
-//console.log("hola mundo");
-//document.write();
 
-/////////////////////////////
+
+//comillas
+//let string1 = "!hola ";
+//let string2 = 'Buenas traddes';
+//let frase = `Este es un saldo: ${string1}`;
+
+
 //concatenacion +
-console.log(ciuduad + " y " + capital + " asdsa ");
+//console.log(ciuduad + " y " + capital + " asdsa ");
 //,
-console.log(ciudad, " y ",capital, " asd " );
+//console.log(ciudad, " y ",capital, " asd " );
+
+
+//prompt
+//let nombre = prompt("Por favor, ingresa tu nombre");
+//let nombre = prompt("Por favor, ingresa tu nombre",0);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 
 //operadores
 
 //suma
-let tomates = 30
-let naranjas = 20
-console.log("La suma entre los tomastes y naranjas ses: ", tomates + naranjas);
+// let tomates = 30
+// let naranjas = 20
+// console.log("La suma entre los tomastes y naranjas ses: ", tomates + naranjas);
+
 
 //suma2
-let caja = "2"
-let estantes = "5"
-console.log(+caja);
-
-suma = console.log("La suma entre caja y estantes es: "+caja + +estantes);
-
+// let caja = "2"
+// let estantes = "5"
+// console.log(+caja);
+// suma = console.log("La suma entre caja y estantes es: "+caja + +estantes);
 //resta - multiplicacion * resto 6 % 2  potencia 2**2=4  
 
-//asignaciones encadenadas
-let a, b, c;
-a = b = c = 5 + 5;
 
-console.log(a,b,c);//10
+//asignaciones encadenadas
+// let a, b, c;
+// a = b = c = 5 + 5;
+// console.log(a,b,c);//10
+
 
 //modifiacion en el lugar
-let n = 1;
-n = n + 9 ;
+// let n = 1;
+// n = n + 9 ;
 //n+= 5;
 //n*=2;
 
 
-////////////////////////
 //incremento decremnento
+// let contador = 1;
+// contador++;
+// contador--;
 
-let contador = 1;
-contador++;
-contador--;
-
-//////////////////
-//CONDICIONALEs////////////////////////
-
-let year = promt ("en que año se creo jvscript", " ")
-//if =comparativo ==operador  ===igualdad estricta
-if(year = 1995){
-    alert("correcto");
-} else{
-    alert("incorrecto");
-}
-
-if(edad >= 18){
-
-}else if(edad == 17){
-
-}else{
-
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-//
-//[1, 2, 3, 4]:forEach(console-log);
 
+//CONDICIONALES
+
+// let year = promt ("en que año se creo jvscript", " ")
+//=comparativo ==operador  ===igualdad estricta
+// if(year = 1995){
+//     alert("correcto");
+// } else{
+//     alert("incorrecto");
+// }
+
+// if(edad >= 18){
+
+// }else if(edad == 17){
+
+// }else{
+
+// }
+
+
+//[1, 2, 3, 4].forEach(console.log);
 //comillas ``
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//CICLOS
+// do{
+
+// }while(condicion);
+
+// while(condicion){
+
+// }
+
+// for(i=0;condiciones;i++){
+
+// }
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//funciones
+// function saludar(){
+//     console.log("hola");
+// }
+// saludar();
+
+// function sumar(a, b){
+//     return a+b;
+// }
+// let resultado = sumar(5, 5);
+
+// let saludar = function(nombre){
+//     console.log("hola ",nombre);
+// }
+// saludar("ju");
+
+// let multiplicar = (a, b)=> a*b;
+// let resultado = multiplicar(5,5);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//objetos
+// let usuario = {
+//     nombre: "Mateo",
+//     age: 30,
+//     city: "Osorno"
+// }
+//agregar datos
+// usuario.estado=true;
 
+//accediendo a una propiedad del objeto
+// console.log(usuario);
 
+//eliminar una propiedad
+// delete usuario.estado
 
