5f9b58f738a53e835ebe1eecf4de8350e313bb0f
kmiloo <camiloandres.lovera@alumnos.ulagos.cl>
2023-10-12 18:16:10 -0300

    R4

--- Retos/R4/JS/jvascript.js
+++ Retos/R4/JS/jvascript.js
@@ -1,11 +1,17 @@
 function Calcular(){
-    let peso=0,altura=0,ims=0;
+    let peso=0,altura=0,imc=0;
     peso = parseFloat(document.getElementById('peso').value);
     altura = parseFloat(document.getElementById('altura').value);
     imc = peso/(altura**2);
+    factor = parseFloat(document.getElementById('actividad').value);
 
+    //Nombre
+    document.getElementById('nomb').innerHTML = "Resutado para "+document.getElementById('nombre').value;
+
+    //IMC
     document.getElementById('imc').innerHTML = imc;
-
+   
+    //Clasificacion
     if(imc<18.5){
         document.getElementById('clasif').innerHTML = "Bajo";
     }else if(24.9<imc &&imc>18.5){
@@ -16,7 +22,15 @@ function Calcular(){
     }else{
         document.getElementById('clasif').innerHTML = "Obeso";
     }
+    //Gastro energetico
+    document.getElementById('gasto').innerHTML = factor*peso;
 
-    
+    //Estrado nutricional
+    if(29.9<imc && imc>25){
+        document.getElementById('estado').innerHTML = "NO necesita atencion especializada";
+    }else{
+        document.getElementById('estado').innerHTML = "Necesita atencion especializada";
+    }
+
     
 }
