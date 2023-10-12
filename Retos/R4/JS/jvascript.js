4d2eb6d64b1c993064c4ad1af848781df35fa883
kmiloo <camiloandres.lovera@alumnos.ulagos.cl>
2023-10-12 18:55:42 -0300

    R4

--- Retos/R4/JS/jvascript.js
+++ Retos/R4/JS/jvascript.js
@@ -12,25 +12,23 @@ function Calcular(){
     document.getElementById('imc').innerHTML = imc;
    
     //Clasificacion
-    if(imc<18.5){
+    if (imc < 18.5) {
         document.getElementById('clasif').innerHTML = "Bajo";
-    }else if(24.9<imc &&imc>18.5){
-
+    } else if (imc >= 18.5 && imc <= 24.9) {
         document.getElementById('clasif').innerHTML = "Normal";
-    }else if(29.9<imc && imc>25){
+    } else if (imc >= 25 && imc <= 29.9) {
         document.getElementById('clasif').innerHTML = "Sobre Peso";
-    }else{
+    } else {
         document.getElementById('clasif').innerHTML = "Obeso";
     }
+    
     //Gastro energetico
-    document.getElementById('gasto').innerHTML = factor*peso;
+    document.getElementById('gasto').innerHTML = factor*peso+ "Kcal";
 
     //Estrado nutricional
-    if(29.9<imc && imc>25){
+    if( 29.9<imc>25 ){
         document.getElementById('estado').innerHTML = "NO necesita atencion especializada";
     }else{
         document.getElementById('estado').innerHTML = "Necesita atencion especializada";
     }
-
-    
 }
